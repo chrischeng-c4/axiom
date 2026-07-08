3ce088f14bb6065331c535950b0af68836c93d23
chrischeng-c4 <chris.cheng.c4@gmail.com>
2026-07-08 13:44:10 +0800

    refactor(mamba): split builtins generic alias helpers

--- projects/mamba/src/runtime/builtins/mod.rs
+++ projects/mamba/src/runtime/builtins/mod.rs
@@ -26,6 +26,7 @@ mod complex_helpers;
 mod errors;
 mod eval_exec;
 mod floor_division;
+mod generic_alias;
 mod hash;
 mod import_hook;
 mod input;
@@ -85,6 +86,7 @@ pub use eval_exec::{
     mb_exec_with_globals, mb_exec_with_globals_locals, mb_globals, mb_locals,
 };
 pub use floor_division::mb_floordiv;
+use generic_alias::{generic_alias_origin_args, generic_alias_repr_string};
 pub use hash::mb_hash;
 pub use import_hook::mb_dunder_import;
 pub use input::mb_input;
@@ -2724,47 +2726,7 @@ fn cell_values_lt(a: MbValue, b: MbValue) -> Option<bool> {
     }
 }
 
-fn generic_alias_origin_args(
-    class_name: &str,
-    fields: &super::rc::MbRwLock<super::rc::InstanceFields>,
-) -> Option<(MbValue, Vec<MbValue>)> {
-    let (origin_key, args_key) = match class_name {
-        "GenericAlias" => ("__origin__", "__args__"),
-        "types.GenericAlias" => ("_origin", "_args"),
-        _ => return None,
-    };
-    let (origin, args_val) = {
-        let g = fields.read().ok()?;
-        (*g.get(origin_key)?, *g.get(args_key)?)
-    };
-    let args = args_val
-        .as_ptr()
-        .map(|ptr| unsafe {
-            match &(*ptr).data {
-                ObjData::Tuple(items) => items.to_vec(),
-                ObjData::List(lock) => lock.read().unwrap().iter().copied().collect(),
-                _ => vec![args_val],
-            }
-        })
-        .unwrap_or_else(|| vec![args_val]);
-    Some((origin, args))
-}
-
-fn type_object_display_name(v: MbValue) -> Option<String> {
-    let ptr = v.as_ptr()?;
-    unsafe {
-        match &(*ptr).data {
-            ObjData::Instance { class_name, fields } if class_name == "type" => fields
-                .read()
-                .ok()
-                .and_then(|f| f.get("__name__").copied())
-                .and_then(str_value),
-            _ => None,
-        }
-    }
-}
-
-fn str_value(v: MbValue) -> Option<String> {
+pub(super) fn str_value(v: MbValue) -> Option<String> {
     let ptr = v.as_ptr()?;
     unsafe {
         match &(*ptr).data {
@@ -2772,30 +2734,6 @@ fn str_value(v: MbValue) -> Option<String> {
             _ => None,
         }
     }
-}
-
-fn generic_alias_repr_part(v: MbValue) -> String {
-    if let Some(name) = pep695_display_name(v) {
-        return name;
-    }
-    if let Some(name) = type_object_display_name(v) {
-        return name;
-    }
-    str_value(mb_repr(v)).unwrap_or_default()
-}
-
-fn generic_alias_repr_string(
-    class_name: &str,
-    fields: &super::rc::MbRwLock<super::rc::InstanceFields>,
-) -> Option<String> {
-    let (origin, args) = generic_alias_origin_args(class_name, fields)?;
-    let name = generic_alias_repr_part(origin);
-    let parts = args
-        .into_iter()
-        .map(generic_alias_repr_part)
-        .collect::<Vec<_>>()
-        .join(", ");
-    Some(format!("{name}[{parts}]"))
 }
 
 /// Try the reflected __eq__ on `obj` (i.e. obj.__eq__(other)).
