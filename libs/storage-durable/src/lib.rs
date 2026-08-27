6fd2c27aa0a5651f5903642501add91d5405ca8d
chrischeng-c4 <chris.cheng.c4@gmail.com>
2026-08-27 14:31:46 +0800

    feat(storage-durable): activate immutable generations

--- libs/storage-durable/src/lib.rs
+++ libs/storage-durable/src/lib.rs
@@ -8,10 +8,17 @@
 mod atomic;
 mod framed_log;
 mod fsync;
+mod generation;
 mod snapshot_store;
 
 pub use atomic::{atomic_write, sync_parent_dir};
 pub use framed_log::{FramedLogReader, FramedLogWriter, LogFrame};
 pub use fsync::FsyncPolicy;
+pub use generation::{
+    CommitError, CommitFailureClass, CommitStep, CurrentReadError, CurrentReadErrorKind,
+    CurrentTarget, FailureInjector, FailurePoint, GenerationName, GenerationNameError,
+    GenerationNameErrorKind, GenerationStore, NoFailures, StagedGeneration, CURRENT_FILE_NAME,
+    CURRENT_TEMP_FILE_NAME, EMPTY_CURRENT_BYTES,
+};
 pub use snapshot_store::{SnapshotFile, SnapshotFileStore};
 // CODEGEN-END
